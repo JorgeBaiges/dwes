30b09812bc0d098b0811c3a897dcf91871cd5f61
Jorge Baiges <joosbaiges@gmail.com>
2022-10-26 17:51:23 +0100

    JSON deseos

---
+++ data/ejercicios/solucionest2/deseos/deseos.php
@@ -0,0 +1,5 @@
+//Lista de deseos
+/*deseos.php => desplegable : vaqueros, telefono, coche, collar, cd
+se van mostrando en la misma pagina lo elegido.
+=> Sesiones, codificarlo con json
+    -> recuperar de la informacion del json codificado */
---
+++ data/ejercicios/solucionest2/sesiones/principal.php
@@ -0,0 +1,4 @@
+<?php
+
+    session_start();
+    print "<h2>Bienvenido usuario : </h2>" . $_SESSION;
